2162a56d7c8c7d747f7b6afa6349b5ae8bf3e755
DekamaxX <dobosz.mateusz1994@gmail.com>
2023-10-11 11:49:49 +0200

    change task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,14 +7,37 @@ const ingredients = [
   'Condiments',
 ];
 
-const ingredientsList = document.querySelector('#ingredients');
+const listIngredients = document.querySelector('#ingredients');
 
-const ingredientsArr = ingredients.map(ingredient => {
-  const listElement = document.createElement('li');
-  listElement.textContent = ingredient;
+const firstItem = document.createElement('li');
+firstItem.textContent = ingredients[0];
+firstItem.classList.add('item');
 
-  listElement.className = 'item';
-  return listElement;
-});
+const secondItem = document.createElement('li');
+secondItem.textContent = ingredients[1];
+secondItem.classList.add('item');
 
-ingredientsList.append(...ingredientsArr);
+const thirdItem = document.createElement('li');
+thirdItem.textContent = ingredients[2];
+thirdItem.classList.add('item');
+
+const forthItem = document.createElement('li');
+forthItem.textContent = ingredients[3];
+forthItem.classList.add('item');
+
+const fifthItem = document.createElement('li');
+fifthItem.textContent = ingredients[4];
+fifthItem.classList.add('item');
+
+const sixthItem = document.createElement('li');
+sixthItem.textContent = ingredients[5];
+sixthItem.classList.add('item');
+
+listIngredients.append(
+  firstItem,
+  secondItem,
+  thirdItem,
+  forthItem,
+  fifthItem,
+  sixthItem
+);
